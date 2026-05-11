0fcfa892b0b0bb86ab660880f4c43bc2c5e2072a
harveyfa <ZhmulevVV@gmail.com>
2026-05-11 23:10:18 +0700

    init

---
+++ run_once_install-oh-my-zsh.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Установка Oh My Zsh, если ещё не установлен
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Устанавливаю Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+# Плагины
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+fi
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+fi
