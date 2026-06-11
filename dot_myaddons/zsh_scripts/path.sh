f8fbd08f3a2ba005aa7c4a9e717aa4f9f89fa152
harveyfa <ZhmulevVV@gmail.com>
2026-06-11 19:56:24 +0700

    some

--- dot_myaddons/zsh_scripts/path.sh
+++ dot_myaddons/zsh_scripts/path.sh
@@ -1,3 +1,4 @@
 export PATH="$HOME/nvim-macos-x86_64/bin:$PATH"
 export PATH="$HOME/bin:$PATH"
 export PATH="$HOME/carbonyl-0.0.3/:$PATH"
+export PATH="/Applications/Postgres.app/Contents/Versions/latest/bin:$PATH"
